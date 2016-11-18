dfc167651f7ebbd3f830e62f80f7a8d8239658f2
Anfifo <afonseca-1995@hotmail.com>
2016-11-18 16:13:26 +0000

    potato

--- intermedia/pex/app/main/EditProgram.java
+++ intermedia/pex/app/main/EditProgram.java
@@ -1,6 +1,9 @@
 package pex.app.main;
 
 //FIXME import used core classes
+
+import pex.app.main.Message;
+
 import pex.app.evaluator.EvaluatorMenu;
 import pt.utl.ist.po.ui.Command;
 import pt.utl.ist.po.ui.InputString;
@@ -12,19 +15,42 @@ import pt.utl.ist.po.ui.Form;
 /**
  * Open menu for managing programs.
  */
-public class EditProgram extends Command</*FIXME Receiver class*/> {
+public class EditProgram extends Command<InterpreterHandler> {
 
     /**
      * @param receiver
      */
-    public EditProgram(/*FIXME core class*/ receiver) {
+    public EditProgram(InterpreterHandler receiver) {
         super(Label.MANAGE_PROGRAM, receiver);
     }
 
     /** @see pt.utl.ist.po.ui.Command#execute() */
     @Override
     public final void execute() {
-        //FIXME implement
-    }
 
+        Form f = new Form();
+        InputString inS = new InputString (f, requestProgramId());
+        f.parse();
+        
+        Display display = new Display();
+
+        name = inS.value();
+        Program program = entity().getInterpreter().getProgram();
+        
+        if ( program == null){
+            display.add(noSuchProgram(name));
+            display.display();
+
+        }
+
+        /* Program Manipulation Menu */
+        else {
+            
+            
+            Command <?> [] commands = {
+                /*
+            };
+        }
+
+    }
 }
--- intermedia/pex/app/main/NewProgram.java
+++ intermedia/pex/app/main/NewProgram.java
@@ -1,6 +1,8 @@
 package pex.app.main;
 
 //FIXME import used core classes
+import pex.app.main.Message
+
 import pt.utl.ist.po.ui.Command;
 import pt.utl.ist.po.ui.Form;
 import pt.utl.ist.po.ui.InputString;
@@ -8,18 +10,23 @@ import pt.utl.ist.po.ui.InputString;
 /**
  * Create new program.
  */
-public class NewProgram extends Command</*FIXME Receiver class*/> {
+public class NewProgram extends Command<InterpreterHandler>{
 
     /**
      * @param receiver
      */
-    public NewProgram(/*FIXME core class*/ receiver) {
+    public NewProgram(InterpreterHandler receiver) {
         super(Label.NEW_PROGRAM, receiver);
     }
 
     /** @see pt.utl.ist.po.ui.Command#execute() */
     @Override
     public final void execute() {
-        //FIXME implement
+        Form f = new Form();
+        InputString name = new InputString (f, requestProgramId());
+        f.parse();
+
+        /* FIXME: Missing treatment for missing program name */
+        entity().getInterpreter().addProgram(entity().getInterpreter, name.value());
     }
 }
