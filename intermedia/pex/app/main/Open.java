d15b565450817b8b3cae1a35d837d83cf6c24c69
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-11-19 15:50:23 +0000

    Update Open.java

--- intermedia/pex/app/main/Open.java
+++ intermedia/pex/app/main/Open.java
@@ -2,10 +2,13 @@ package pex.app.main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Serializable;
+import java.io.File;
 
 //FIXME import used core classes
 import pex.core.InterpreterHandler;
 
+import pex.app.main.Message;
 import pt.utl.ist.po.ui.Command;
 import pt.utl.ist.po.ui.Display;
 import pt.utl.ist.po.ui.Form;
@@ -25,7 +28,59 @@ public class Open extends Command<InterpreterHandler> {
 
     /** @see pt.tecnico.po.ui.Command#execute() */
     @Override
-    public final void execute() throws InvalidOperation {
-        //FIXME implement
+    public final void execute() throws InvalidOperation, IOException, ClassNotFoundException {
+        Interpreter inter = null;
+        int i = 0;
+        
+        try{
+            Form f = new Form();
+            InputString name = new InputString(f, Message.openFile());
+            f.parse();    
+
+            File file = null;
+
+            file = new File("name.txt");
+
+            if (file.exists()) {
+
+                FileInputStream file = new FileInputStream("name.ser");
+                ObjectInputStream in = new ObjectInputStream(file);
+                inter = (Interpreter) in.readObject();
+                in.close();
+                file.close();
+            }
+
+            else{
+                Display display = new Display();
+
+                while( !file.exists() ){
+
+                display.add(Message.fileNotFound());
+                display.display();
+
+                InputString new_name = new InputString(f, Message.openFile());
+                file = new File("new_name.txt");
+
+                }
+
+                FileInputStream file = new FileInputStream("new_name.ser");
+                ObjectInputStream in = new ObjectInputStream(file);
+                inter = (Interpreter) in.readObject();
+                in.close();
+                file.close();
+            }
+        }
+
+        catch(InvalidOperation o){
+            
+        }
+        catch(IOException i){
+            
+        }
+        catch(ClassNotFoundException c){
+            
+        }
+            
+
     }
 }
