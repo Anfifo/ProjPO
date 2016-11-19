0cfb02336664baf02c387ddf8e4191c0f1146c60
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-11-19 15:50:56 +0000

    Update Save.java

--- intermedia/pex/app/main/Save.java
+++ intermedia/pex/app/main/Save.java
@@ -1,8 +1,11 @@
 package pex.app.main;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.io.File;
 
 //FIXME import used core classes
+import pex.app.main.Message;
 import pt.utl.ist.po.ui.Command;
 import pt.utl.ist.po.ui.Form;
 import pt.utl.ist.po.ui.InputString;
@@ -11,17 +14,54 @@ import pt.utl.ist.po.ui.InvalidOperation;
 /**
  * Save to file under current name (if unnamed, query for name).
  */
-public class Save extends Command</*FIXME Receiver class*/> {
+public class Save extends Command<InterpreterHandler> {
     /**
      * @param receiver
      */
-    public Save(/*FIXME core class*/ receiver) {
+    public Save(InterpreterHandler receiver) {
         super(Label.SAVE, receiver);
     }
 
     /** @see pt.utl.ist.po.ui.Command#execute() */
     @Override
-    public final void execute() throws InvalidOperation {
-        //FIXME implement
+    public final void execute() throws InvalidOperation, IOException {
+        try{
+
+            Form f = new Form();
+            InputString name = new InputString(f, Message.saveAs());
+            f.parse();
+            File file = null;
+
+            file = new File("name.txt");
+            if ( !file.exists() ) {
+                Form f = new Form();
+                InputString name = new InputString(f, Message.newSaveAs());
+                f.parse
+
+                file = File("name.txt");
+            }
+
+            if( !(file.createNewFile()) ){
+                file.delete();
+                file.createNewFile();
+            }
+
+           
+
+            FileOutputStream file = new FileOutputStream("name.ser");
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(entity().getInterpreter() );
+            out.close();
+            file.close();
+
+        }
+
+        catch(InvalidOperation o){
+            
+        }
+        catch(IOException i){
+            
+        }
+
     }
 }
