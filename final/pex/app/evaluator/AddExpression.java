ffb0d6947f2deffc44f66e1055231aa49c31c19e
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-12-08 09:34:49 -0800

    Update AddExpression.java

--- final/pex/app/evaluator/AddExpression.java
+++ final/pex/app/evaluator/AddExpression.java
@@ -5,6 +5,9 @@ import pex.app.BadExpressionException;
 import pex.app.BadPositionException;
 
 import pex.core.Program;
+import pex.parser.NewParser;
+import pex.core.expression.Expression;
+import pex.parser.ParserException;
 
 import pt.utl.ist.po.ui.Form;
 import pt.utl.ist.po.ui.InputString;
@@ -24,6 +27,28 @@ public class AddExpression extends ProgramCommand {
     /** @see pt.utl.ist.po.ui.Command#execute() */
     @Override
     public final void execute() throws BadExpressionException, BadPositionException {
-        //FIXME implement
+        
+        Form f = new Form();
+        InputInteger pos = new InputInteger (f, Message.requestPosition());
+        InputString exp = new InputString (f, Message.requestExpression());
+        f.parse();
+
+        try{
+            
+            NewParser parser = new NewParser();
+
+            int position = pos.value();
+
+            if ( position >= entity().getExpressions().size() || position < 0 ) {
+                throw new BadPositionException(position);
+            }
+
+            Expression expression = parser.parseString(exp.value(), entity());
+
+            entity().add(position, expression);
+        }
+
+         catch(BadPositionException p){  }
+         catch(ParserException e){ throw new BadExpressionException(exp.value()); }
     }
 }
